09517c680bc2f21f1e2d6ab1f816540c5080a0d3
Fernando Mayer <fernandomayer@gmail.com>
2019-12-13 01:08:19 -0300

    Manda PDF para servidor

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ read -p 'PORTA: ' PORT
 
 Rscript -e 'rmarkdown::render("rufpr.Rmd")'
 rsync -avz -e "ssh -p $PORT" rufpr.html ${USER}@${HOST}:~/${DIR}/index.html
-# rsync -avz -e "ssh -p $PORT" rufpr.pdf ${USER}@${HOST}:~/${DIR}/rufpr.pdf
+rsync -avz -e "ssh -p $PORT" rufpr.pdf ${USER}@${HOST}:~/${DIR}/rufpr.pdf
 
 exit 0
